bd09065e8c3ca33d993fe1d749f2af4dc085a05f
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2025-09-28 00:37:00 +0700

    some small stuff

---
+++ Common/Wrapper/StructureHelperWrapper.cs
@@ -0,0 +1,19 @@
+﻿using StructureHelper.API;
+using StructureHelper.Models;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace ModJam2.Common.Wrapper
+{
+    public static class StructureHelperWrapper
+    {
+        public static void Get_StructureData(string path, Mod mod)
+        {
+            Generator.GetStructureData(path, mod);
+        }
+        public static void Generate(StructureData data, Point16 pos)
+        {
+            Generator.GenerateFromData(data, pos);
+        }
+    }
+}
